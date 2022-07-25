e071b9199978534855af8099b5d908e6aaceaba8
Grant Beard <Grantxbeard@gmail.com>
2022-07-25 15:53:52 -0600

    add controlled form handleChange and useState hook"

--- src/Components/CardForm/CardForm.tsx
+++ src/Components/CardForm/CardForm.tsx
@@ -1,17 +1,32 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 import './CardForm.css';
 
 const CardForm: FC = () => {
-  
+
+    const [to, setTo] = useState<string>('')
+    const [quote, setQuote] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+    const [from, setFrom] = useState<string>('')
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+        if (event.target.name === 'to-input') {
+            setTo(event.target.value)
+        } else if (event.target.name === 'message-input') {
+            setMessage(event.target.value) 
+        } else {
+            setFrom(event.target.value)
+        }
+    } 
+
     return (
         <form>
             <label htmlFor='to-input'>To: 
-                <input type='text' name='to-input' data-cy='to-input'/> 
+                <input type='text' name='to-input' value={to} onChange={ event => handleChange(event)} data-cy='to-input'/> 
             </label>
             <h2></h2>
-            <textarea placeholder='Add message here...' name='message-input' data-cy='message-input'/>
+            <textarea placeholder='Add message here...' name='message-input' onChange={ event => handleChange(event)} value={message} data-cy='message-input'/>
             <label htmlFor='from-input'>From: 
-                <input type='text' name='from-input' data-cy='from-input'/> 
+                <input type='text' name='from-input' onChange={ event => handleChange(event)} value={from} data-cy='from-input'/> 
             </label>
         </form>
     );
