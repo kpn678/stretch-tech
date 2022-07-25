68e344e44e50d8efe40cb8502d1480eaf27caec8
Grant Beard <Grantxbeard@gmail.com>
2022-07-25 14:06:09 -0600

    Add routes to main page

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,21 +1,29 @@
 import React, { FC } from 'react';
 import './App.css';
+import { Route } from "react-router-dom"
 import NavBar from '../NavBar/NavBar';
 import CardForm from '../CardForm/CardForm';
 import CardPreview from '../CardPreview/CardPreview';
 import SavedCards from '../SavedCards/SavedCards';
+
 
 const App: FC = () => {
 
   return (
     <main>
       <NavBar />
-      <section className='home' data-cy='home'>
-
-      </section>
-      <CardForm />
-      <CardPreview />
-      <SavedCards />
+      <Route 
+       path='/' render={()=> <section className='home' data-cy='home'></section>}
+      />
+      <Route
+      path='/create-card' render={() => <CardForm />}
+      />
+      <Route
+      path='/preview-card' render={() => <CardPreview />}
+      />
+      <Route
+      path='/saved-cards' render={() => <SavedCards />}
+      />
     </main>
   );
    
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom'
 import './NavBar.css';
 
 
 const NavBar: FC = () => {
     return (
-        <h1>Corndogs!</h1>
+        <Link to={'/'} style={{textDecoration: 'none'}}>
+            <h1>Card Generator</h1>
+        </Link>
     );
 };
 
