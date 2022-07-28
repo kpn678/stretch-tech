3564356755bd0a198e0a461e1558b3e9c3e59be2
Cami Anderson <camidantas@icloud.com>
2022-07-28 18:21:00 -0400

    Add test for card generator flow

---
+++ cypress/e2e/joke-card-form.cy.ts
@@ -0,0 +1,41 @@
+describe('Joke Card Form Flow', () => {
+    beforeEach(()=> {
+      cy.intercept('GET', 'https://geek-jokes.sameerkumar.website/api?format=json', {
+        fixture: "joke.json",
+        statusCode: 200
+      })
+      cy.visit('http://localhost:3000/')
+      cy.get('[data-cy="jokes-button"]').click()
+    })
+    it('Should display a header and Joke Card form', () => {
+      cy.get('form')
+      cy.contains('h1', 'Card Generator')
+    })
+    it('Should be able to generate a new joke', () => {
+      cy.intercept('GET', 'https://geek-jokes.sameerkumar.website/api?format=json', {
+        fixture: "joke2.json",
+        statusCode: 200
+      })
+      cy.get('[data-cy="get-quote-button"]').click()
+      cy.get('form').within(() => {
+        cy.contains('[data-cy="quote"]', 'Love does not hurt. Chuck Norris does.')
+      })
+    })
+    it('Should be able to fill out input fields', () => {
+      cy.get('form').within(() => {
+        cy.get('[data-cy="to-input"]').type('Scott')
+        cy.contains('[data-cy="quote"]', 'I would love to change the world but they won’t give me the source code.')
+        cy.get('[data-cy="message-input"]').type('U R GR8!!!!!!!')
+        cy.get('[data-cy="from-input"]').type('A. C. G. K.')
+      })
+    })
+    it('Should be able to make a card and go to Card Preview Page', () => {
+      cy.get('form').within(() => {
+        cy.get('[data-cy="to-input"]').type('Scott')
+        cy.get('[data-cy="message-input"]').type('U R GR8!!!!!!!')
+        cy.get('[data-cy="from-input"]').type('A. C. G. K.')
+        cy.get('[data-cy="make-card-button"]').click()
+      })
+      cy.url().should('eq', 'http://localhost:3000/preview-card')
+    })
+  })
