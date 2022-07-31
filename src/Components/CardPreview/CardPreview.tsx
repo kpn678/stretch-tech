04763f9954667107f39bc63d444025c936a26931
Amanda Sands <amandajean555@gmail.com>
2022-07-30 19:18:01 -0600

    Install packages to allow card to be downloaded as PDF

--- src/Components/CardPreview/CardPreview.tsx
+++ src/Components/CardPreview/CardPreview.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react'
 import './CardPreview.css'
 import Card from "../../types/Card.type"
 import { Link } from "react-router-dom"
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 
 interface Props {
     currentCard: {
@@ -14,9 +16,28 @@ interface Props {
 }
 
 const CardPreview: FC<Props> = ({currentCard, saveCard}) => {
+    const printRef = React.useRef<HTMLDivElement>(null);
+    console.log(printRef.current)
+
+    const handleDownloadPdfTest= async () => {
+        console.log(printRef.current)
+        const element = printRef.current;
+        const canvas = await html2canvas(element as HTMLDivElement);
+        const data = canvas.toDataURL('image/png');
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight =
+      (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    pdf.save('print.pdf');
+    }
+
     return (
         <section className="card-preview-page">
-            <div className="card">
+            <div className="card" ref={printRef}>
                 <h2 className='to-field'>To: {currentCard.to} </h2>
                 <p className='quote-field'>{currentCard.quote}</p>
                 <p className='message-field'>{currentCard.message}</p>
@@ -29,6 +50,7 @@ const CardPreview: FC<Props> = ({currentCard, saveCard}) => {
                 <Link to="/saved-cards">
                     <button className='save-card-button' onClick={() => saveCard(currentCard)}>Save my card!</button>
                 </Link>
+                <button className='pdf-button' type="button" onClick={handleDownloadPdfTest}> Print to PDF! </button>
             </div>
         </section>
     )
